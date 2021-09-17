9696fb4f99c1365c4cfa53e01008530bee599d19
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:08:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Welcome, new coder. Enter your code on line 2 to print 'Hello, World!' to the console (the panel on the right)
+
+
+
+
+# Next, PLAY! Modify your code on line 2 to try to accomplish the tasks listed in the "Your First Program" page.
